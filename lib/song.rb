eb144224349ab0d84eda10218fa9e6a72ad26bc9
Travis Fox <travis.fox90@gmail.com>
2020-08-02 04:35:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,15 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramble::InstanceMethods
+
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+#  def initialize
+#    @@songs << self
+#  end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -18,19 +22,8 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
